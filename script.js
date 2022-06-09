c92ed4ecc39aa4f8b07335f90c7547de1e295392
Localhost <102569027+maodesigns@users.noreply.github.com>
2022-06-09 17:46:54 +0100

    Updated
    Some comments were added

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function setCookie(cookieName, cookieValue, minutes) {
     if (minutes) {
+        // set Expiry time
         var date = new Date();
         date.setTime(date.getTime() + (minutes * 60 * 1000));
         var expires = "; expires=" + date.toGMTString();
     } else {
         var expires = "";
     }
-    document.cookie = `${cookieName}=${cookieValue}${expires}; path=/`;
+    document.cookie = `${cookieName}=${cookieValue}${expires}; path=/`; //set cookie with expiry time
 }
 
 function getCookieValue(cookieName) {
@@ -25,9 +26,10 @@ function getCookieValue(cookieName) {
     }
 }
 
+// Check if a user exists
 function checkUser() {
     let username = getCookieValue("userId");
-    let previousPoint = getCookieValue("point");
+    let previousPoint = getCookieValue("point");  // gets user previous point
     if (username != "") {
         let response = prompt(`Welcome again ${username}! You have ${previousPoint} points
 
@@ -38,7 +40,7 @@ Enter "yes" to proceed OR "no" to quit
 NB: Your Game information expires soon`).toLowerCase()
         while (response != 'no') {
             if (response === 'yes') {
-                startGuessGame(username);
+                startGuessGame();
                 break
             } else if (response === 'no') {
                 stop;
@@ -63,7 +65,7 @@ NB: Your Game information expires after 5mins.`)
 }
 
 function secretNumber(min, max) {
-    return ((Math.random() * (max - min) + min).toFixed(1))  // returns random number to  1 decimal place
+    return ((Math.random() * (max - min) + min).toFixed(1))  // returns random number to 1 decimal place
 }
 
 function startGuessGame() {
@@ -79,7 +81,7 @@ Hint: Your guess should be to 1 decimal place`);
 function checkUserGuess(userGuess, mySecretNumber) {
     while (userGuess != 'q') {
         if (parseFloat(userGuess) == mySecretNumber) {
-            moveToNextStage()
+            moveToNextStage()   // moves user to next stage
             break
         } else if (parseFloat(userGuess) > mySecretNumber) {
             userGuess = prompt(`Your guess is too high, Try again. Take a guess between 1 and ${max} || Enter "q" to quit `)
@@ -92,14 +94,16 @@ function checkUserGuess(userGuess, mySecretNumber) {
 }
 
 function moveToNextStage() {
-    previousPoint = getCookieValue('point')
-    setCookie('point', parseInt(previousPoint) + 5);
-    newPoint = parseInt(getCookieValue('point'));
-    max = max + 1;
+    previousPoint = getCookieValue('point')  // gets user previous point
+    setCookie('point', parseInt(previousPoint) + 5); // updates user's point
+    newPoint = parseInt(getCookieValue('point')); // gets user's updated point 
+    max = max + 1;  // increases the range 
+    
     alert(`Correct!!
 
 Congratulations on to the next stage. You just gained ${newPoint} points!`
     );
+    
     let userGuess = prompt(`Take a guess between 1 and ${max} or Enter "q" to quit`);
     let myNewSecretNumber = secretNumber(min, max);
     console.log(`My New Secret Number = ${myNewSecretNumber}`)
@@ -113,3 +117,4 @@ let point = 0;
 
 const mySecretNumber = secretNumber(min, max)
 console.log(`My Secret Number = ${mySecretNumber}`)
+checkUser()
